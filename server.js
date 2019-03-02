3ea25785dfb92473612903559ae450323f8363a3
thurainhein <thurainhein097@gmail.com>
2019-03-02 17:35:49 +0630

    build for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+app.use(express.static(__dirname + '/dist/'))
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/dist/index.html');
+})
+
+
+app.listen(port, () => {
+    console.log('Server is running ...');
+
+})
